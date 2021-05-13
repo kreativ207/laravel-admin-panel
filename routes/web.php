774c69d514e4ebeb3a0a3eb54eed7c42b2fd0733
Nikolay <nikolaynigorodov@gmail.com>
2021-05-13 15:48:33 +0300

    middelware check user on role

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::middleware(['role:admin'])->prefix('admin_panel')->group(function () {
+Route::middleware(['admin.middleware:admin,/home', 'role:admin'])->prefix('admin_panel')->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('homeAdmin');
     Route::resource('/category', CategoryController::class);
 });
